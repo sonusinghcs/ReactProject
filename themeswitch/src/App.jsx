18b4d39d05f2a8a602265d7ebb67978a6829989f
sonu-singh <sonusinghanpcs@gmail.com>
2024-08-14 03:43:03 +0530

    context api

---
+++ themeswitch/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState ,useEffect} from 'react'
+
+
+import { ThemeProvider } from './contexts/Theme'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/Card'
+
+function App() {
+  const [themeMode, setThemeMode] = useState("light")
+
+  const lightTheme =()=>{
+    setThemeMode("light")
+  }
+  const darkTheme = ()=>{
+    setThemeMode("dark")
+  }
+  useEffect(() => {
+    document.querySelector("html").classList.remove("dark","light");
+    document.querySelector("html").classList.add(themeMode);
+  }, [themeMode])
+  
+
+  return (
+   <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
+<div className="flex flex-wrap min-h-screen items-center">
+    <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/* themebtn */}
+            <ThemeBtn/>
+        </div>
+
+        <div className="w-full max-w-sm mx-auto">
+           {/* card */}
+           <Card/>
+        </div>
+    </div>
+</div>
+</ThemeProvider> 
+
+  )
+}
+
+export default App
